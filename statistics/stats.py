13a3046978a8983763a636373fe79d0164202e71
Starkiller13 <andrea.costalonga@outlook.com>
2022-02-06 00:15:16 +0100

    added a montecarlo sim

---
+++ statistics/stats.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+file = open("../mc_sim.txt","r").readlines()
+data = []
+for i in file:
+    arr = i[1:-2].split(", ")
+    arr = [float(j) for j in arr]
+    data.append(arr)
+    
+data = np.array(data)
+
+for index in range(len(data[0])):
+    mu = np.mean(data[:,index])
+    std = np.std(data[:,index])
+    x = np.arange(mu - 3*std, mu + 3*std, 0.001)
+    plt.hist(data[:,index],density=True)
+    plt.plot(x, norm.pdf(x, mu, std))
+    plt.show()
