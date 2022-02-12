46e846da0d45b37b7d1d76badcea0816a42d66ed
Starkiller13 <andrea.costalonga@outlook.com>
2022-02-12 23:30:55 +0100

    some changes

--- statistics/stats.py
+++ statistics/stats.py
@@ -1,21 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-file = open("../results/mc_sim.txt","r").readlines()
+
+file = open("../results/mc_sim3.txt","r").readlines()
 data = []
 for i in file:
     arr = i[1:-2].split(", ")
     arr = [float(j) for j in arr]
     data.append(arr)
-    
+
 data = np.array(data)
 
+means = []
+sigmas = []
 for index in range(len(data[0])):
     mu = np.mean(data[:,index])
     std = np.std(data[:,index])
-    x = np.arange(mu - 3*std, mu + 3*std, 0.001)
+    means.append(mu)
+    sigmas.append(std)
+    '''
+    x = np.arange(mu - 4*std, mu + 4*std, 0.001)
+    
     plt.hist(data[:,index],bins = 20, density=True)
     plt.plot(x, norm.pdf(x, mu, std))
+    plt.legend(["mean: %.2f \nstd %.2f"%(mu,std)],loc = 'upper left')
+    plt.xlabel("occurrencies")
+    plt.ylabel("density")
     #plt.show()
     plt.savefig("img/plot_"+str(index)+".png")
     plt.close()
+    '''
+
+print("expectations: "+str(["%.4f"%item for item in means]))
+print("stds :"+str(["%.4f"%item for item in sigmas]))
+
+mot = [153801, 36648, 65143, 3706, 440712, 169731, 79]#, 2910, 2048, 729665, 1621, 9377, 13752]
+
+zscore = []
+pvalue = []
+
+for i in range(0,7):
+    z = (mot[i]-means[i])/sigmas[i]
+    zscore.append(z)
+
+print("z-scores: "+str(["%.4f"%item for item in zscore]))
+
+for i in zscore:
+    p = norm.pdf(abs(i))
+    pvalue.append(p)
+
+print("p-values: "+str(["%.4f"%item for item in pvalue]))
